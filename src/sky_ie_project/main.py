3d12fbf0839631c4a74e208e41b814f99d73ea42
류근호 <keunho@root-MacBookPro.local>
2025-07-04 20:51:25 +0900

    Update

--- src/sky_ie_project/main.py
+++ src/sky_ie_project/main.py
@@ -4,9 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 # Plotting the line of total demand over time regardless of the product and the city
-def total_demand_plot():
-    # Connect to the SQLite database
-    conn = sqlite3.connect("data/demand_train.db")
+def total_demand_plot(conn):
     
     # Query to aggregate total demand by date from the database
     query = """SELECT date, SUM(demand) as total_demand
@@ -17,9 +15,6 @@ def total_demand_plot():
     
     # Read the query result into a DataFrame
     df = pd.read_sql_query(query, conn)
-    
-    # Close the database connection
-    conn.close()
     
     # Convert 'date' column to datetime format
     df["date"] = pd.to_datetime(df["date"])
@@ -36,7 +31,7 @@ def total_demand_plot():
     # Adding labels and title
     plt.xlabel("Date")
     plt.ylabel("Total Demand")
-    plt.title("Monthly Total Demand")
+    plt.title("Total Demand")
     plt.grid(True)
     plt.tight_layout()
     
@@ -44,23 +39,18 @@ def total_demand_plot():
     plt.show()
 
 # Plotting the line of total demand over time of the city regardless of the product
-def city_demand_plot(which_city):
-    # Connect to the SQLite database
-    conn = sqlite3.connect("data/demand_train.db")
+def city_demand_plot(which_city, conn):
     
     # Query to aggregate total demand by date from the database
     query = """SELECT date, SUM(demand) as total_demand
     FROM demand_train
-    WHERE city = "Montreal"
+    WHERE city = ?
     GROUP BY date
     ORDER BY date ASC
     """
     
     # Read the query result into a DataFrame
-    df = pd.read_sql_query(query, conn)
-    
-    # Close the database connection
-    conn.close()
+    df = pd.read_sql_query(query, conn, params=(which_city,))
     
     # Convert 'date' column to datetime format
     df["date"] = pd.to_datetime(df["date"])
@@ -77,19 +67,22 @@ def city_demand_plot(which_city):
     # Adding labels and title
     plt.xlabel("Date")
     plt.ylabel("Total Demand")
-    plt.title("Monthly Total Demand")
+    plt.title(f"Total Demand in {which_city}")
     plt.grid(True)
     plt.tight_layout()
     
     # Show the plot
     plt.show()
-
-
-
-def main():
-    #total_demand_plot()
-    city_demand_plot("Montreal")
     
+def main():
+    # Connect to the SQLite database
+    conn = sqlite3.connect("data/demand_train.db")
+    
+    total_demand_plot(conn)
+    city_demand_plot("Montreal", conn)
+
+    # Close the database connection
+    conn.close()    
 
 
 if __name__ == "__main__":
